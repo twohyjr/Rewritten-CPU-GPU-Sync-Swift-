0803291959201b80fcd087b0c7ee71d00276885f
Rick Twohy <twohyjr@gmail.com>
2017-10-21 11:42:34 -0500

    Initial Commit

---
+++ Rewritten CPU-GPU Sync (Swift)/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Rewritten CPU-GPU Sync (Swift)
+//
+//  Created by Rick Twohy on 10/21/17.
+//  Copyright © 2017 Rick Twohy. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Rewritten CPU-GPU Sync (Swift)/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Rewritten CPU-GPU Sync (Swift)
+//
+//  Created by Rick Twohy on 10/21/17.
+//  Copyright © 2017 Rick Twohy. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
